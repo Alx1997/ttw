28a05928f4d95f347ada42ff48eb49972c07b139
Alx1997 <atchere@yandex.ru>
2020-10-15 20:00:11 +0300

    new

--- src/main/java/com/translatetheword/translatetheword/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.translatetheword.translatetheword.repo;
-
-
-import com.translatetheword.translatetheword.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
